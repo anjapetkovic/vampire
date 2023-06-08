4060869eb18777394ccdc0b18f7ee31f299f24a3
Michael Rawson <michael@rawsons.uk>
2023-06-08 21:44:49 +0100

    towards proof printing with Dedukti; buggy somehow

--- Inferences/BinaryResolution.cpp
+++ Inferences/BinaryResolution.cpp
@@ -182,6 +182,8 @@ Clause* BinaryResolution::generateClause(
       }
       resLits->push(newLit);
     }
+    else
+      leftIndex = i;
   }
 
   Literal* qrLitAfter = 0;
@@ -216,6 +218,8 @@ Clause* BinaryResolution::generateClause(
       }
       resLits->push(newLit);
     }
+    else
+      rightIndex = i;
   }
 
   if (!absUnif->usesUwa()) {
